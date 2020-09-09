d72deb119f568c45accfa390cc4312b361dbf162
Taylor Epskamp <taylorepskamp@gmail.com>
2020-09-09 13:37:54 -0400

    Updates

---
+++ precache-manifest.2c1725d77d4275af6d1d6059643eac0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb5fa1a8999a5b5e116aab7051d50d8",
+    "url": "/diceify/index.html"
+  },
+  {
+    "revision": "a5e8bc7d3debb2012e61",
+    "url": "/diceify/static/css/main.5b947da8.chunk.css"
+  },
+  {
+    "revision": "3295d09cf185f2a782b8",
+    "url": "/diceify/static/js/2.138e7ba5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/diceify/static/js/2.138e7ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e8bc7d3debb2012e61",
+    "url": "/diceify/static/js/main.eee6c1fd.chunk.js"
+  },
+  {
+    "revision": "121ec4b08a4a06c208ea",
+    "url": "/diceify/static/js/runtime-main.1f6671dc.js"
+  }
+]);
